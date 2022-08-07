56024fcbbf6b2f85c41fbaf54bcab5da7da3ed2c
Hassan Ashraf <hassanelking92@gmail.com>
2022-08-07 10:08:02 +0200

    make navigations tabs screen

---
+++ lib/tabs/Radio.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class RadioScreen extends StatelessWidget {
+  const RadioScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/tabs/ahadeth.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class AhadethScreen extends StatelessWidget {
+  const AhadethScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,
+
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
