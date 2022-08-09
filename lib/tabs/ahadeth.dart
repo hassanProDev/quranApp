4ea6cb7784e2dbfb40a5b4c8030defea645dcd5e
Hassan Ashraf <hassanelking92@gmail.com>
2022-08-09 11:07:15 +0200

    add number of ayat

--- lib/tabs/ahadeth.dart
+++ lib/tabs/ahadeth.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:islami_app/theming/my_them_mood.dart';
 
 class AhadethScreen extends StatelessWidget {
   const AhadethScreen({Key? key}) : super(key: key);
@@ -7,11 +7,30 @@ class AhadethScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purpleAccent,
-
+      backgroundColor: Colors.transparent,
       body: Column(
         children: [
-
+          Image.asset('assets/images/ahadethImage.png'),
+          Container(
+            height: 3,
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            color: MyTheme.primaryColor,
+          ),
+          Text(
+            'الاحاديث',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Container(
+            height: 3,
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            color: MyTheme.primaryColor,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 0,
+              itemBuilder: (context, index) => Container(),
+            ),
+          ),
         ],
       ),
     );
